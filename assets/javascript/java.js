ad1f07b9852d8eaa4504601d6418a9a5761e114d
Jamie Scovern <jamie.scovern@gmail.com>
2015-10-22 22:47:22 -0400

    got the nav bar default hide then toggle to open working

---
+++ assets/javascript/java.js
@@ -0,0 +1,9 @@
+$("#toggleContentOnClick").click(function() {
+	$("ul").slideToggle();
+	console.log("made it into the div");
+});
+
+
+// $('#toggle-content-onclick').click(function() {
+  // $('#some_paragraph').slideToggle();
+// });
